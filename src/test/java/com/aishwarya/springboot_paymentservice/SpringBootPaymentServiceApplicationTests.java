bf70b5d980d1d0186b9945ef2b9643025bf16b71
Aishwarya Rangoonwala <aishwaryarangoonwala@Aishwaryas-Laptop.local>
2025-10-23 09:37:00 +0530

    Initialise Payment Mimicroservice

---
+++ src/test/java/com/aishwarya/springboot_paymentservice/SpringBootPaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aishwarya.springboot_paymentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPaymentServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
